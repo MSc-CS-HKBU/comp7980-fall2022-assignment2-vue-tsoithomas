a7298a3c3cdf5da7c6a0544fab1f8f2c18e5e46f
Thomas Tsoi <tommasinino@yahoo.com>
2022-11-02 03:55:34 +0800

    Build HomeView

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import VueApexCharts from "vue3-apexcharts";
 
 import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
 import router from './router'
 
 createApp(App).use(router).use(VueApexCharts).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+
+    <nav id="navbar-example2" class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a class="nav-link" href="#one">one</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#two">two</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#three">three</a>
+        </li>
+      </ul>
+    </nav>
+    <div id="content" data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+      <h4 id="one">one</h4>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+      <h4 id="two">two</h4>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+      <h4 id="three">three</h4>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+      <p>...</p>
+    </div>
+
   </div>
 </template>
 
+
+<style>
+
+#content {
+  height: 400px;
+  overflow-y: scroll;
+}
+</style>
+
+
+
+
 <script>
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HomeForm from '@/components/HomeForm.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    //HomeForm
   }
 }
 </script>
