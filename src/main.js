304897624064cd1bdf7a52a2724dd1f284f80a08
Thomas Tsoi <tommasinino@yahoo.com>
2022-11-02 00:33:48 +0800

    Install Router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import { router } from 'vue'
 import App from './App.vue'
 import VueApexCharts from "vue3-apexcharts";
 
 import "bootstrap/dist/css/bootstrap.min.css"
+import router from './router'
 
 createApp(App).use(router).use(VueApexCharts).mount('#app')
