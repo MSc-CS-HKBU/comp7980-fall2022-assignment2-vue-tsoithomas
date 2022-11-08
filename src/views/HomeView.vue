4bce83704e9db9307d38d4269f75e37b8a7be2aa
Thomas Tsoi <tommasinino@yahoo.com>
2022-11-08 17:34:40 +0800

    Implement AJAX

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>COMP7270 Web and Mobile Programming</h1>
+  <h1 name="">COMP7270 Web and Mobile Programming</h1>
 
   <div class="container">
 
@@ -49,26 +49,79 @@
               <div class="form-group row mb-3">
                 <label for="inputEmail3" class="col-sm-3 col-form-label">Experience (years)</label>
                 <div class="col-sm-9">
-                  <input type="number" class="form-control" id="experience" min="0" max="100">
+                  <div class="input-group">
+                    <input type="number" class="form-control" name="experience" id="experience" min="0" max="100" placeholder="1" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                      <div class="input-group-text" id="basic-addon2">year(s)</div>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div class="form-group row mb-3">
                 <div class="col-sm-3">Preferred language</div>
                 <div class="col-sm-9">
-                  <select class="form-control" name="langauge">
+                  <select class="form-control form-select" name="language">
+                    <option value="" disabled selected>Select your preferred language</option>
+                    <option>C++</option>
+                    <option>Java</option>
                     <option>JavaScript</option>
-                    <option>JavaScript</option>
-                    <option>JavaScript</option>
+                    <option>Objective C</option>
+                    <option>PHP</option>
+                    <option>Python</option>
                   </select>
                 </div>
               </div>
               <div class="form-group row mb-3">
                 <div class="col-sm-10">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="submit" class="btn btn-primary" @click="myMethod">Submit Data & View Charts</button>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-              
+
+
+
+      <div id="one" class="row justify-content-md-center section">
+        <div class="col col-lg-8">
+          <div class="card">
+            <div class="card-header text-center">
+              chart one
+            </div>
+            <div class="card-body text-start">
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div id="two" class="row justify-content-md-center section">
+        <div class="col col-lg-8">
+          <div class="card">
+            <div class="card-header text-center">
+              chart two
+            </div>
+            <div class="card-body text-start">
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="three" class="row justify-content-md-center section">
+        <div class="col col-lg-8">
+          <div class="card">
+            <div class="card-header text-center">
+              chart three
+            </div>
+            <div class="card-body text-start">
+
+
             </div>
           </div>
         </div>
@@ -97,6 +150,14 @@ nav.navbar {
   z-index: 100;
 }
 
+.navbar-brand {
+  text-transform:uppercase;
+}
+
+li.nav-item {
+  font-variant: small-caps;
+}
+
 h1 {
   background: rgb(59,103,158);
   background: -moz-linear-gradient(top,  rgba(59,103,158,1) 0%, rgba(43,136,217,1) 50%, rgba(32,124,202,1) 51%, rgba(125,185,232,1) 100%);
@@ -119,6 +180,7 @@ h1 {
   margin-bottom: 20px;  
   
   min-height: 40rem;
+  padding-bottom: 120px;
 }
 
 .container {
@@ -143,11 +205,18 @@ h1 {
   font-variant: small-caps;
 }
 
-.chart {
-  display: none;
+.card-body {
+  min-height: 15rem;
 }
 
+.chart {
+  /* display: none; */
+}
 
+.input-group-append .input-group-text {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
 
 
 </style>
@@ -159,11 +228,60 @@ h1 {
 
 // @ is an alias to /src
 //import HomeForm from '@/components/HomeForm.vue'
+import { ref } from "vue";
 
 export default {
   name: 'HomeView',
   components: {
     //HomeForm
+  },
+  
+  methods: {
+    myMethod () {
+      var fetch_status;
+
+      fetch('/api/submit', {
+          method: "POST",
+          // Set the headers
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          },
+          // Set the post data
+          body: JSON.stringify({
+              gender : document.querySelector('input[name="gender"]:checked').value,
+              experience : document.querySelector('input[name="experience"]').value,
+              language : document.querySelector('select[name="language"]').value
+          })
+      })
+      .then(function (response) {
+          // Save the response status in a variable to use later.
+          fetch_status = response.status;
+          // Handle success
+          // eg. Convert the response to JSON and return
+          return response.json();
+      }) 
+      .then(function (json) {
+          // Check if the response were success
+          if (fetch_status == 201) {
+              // Use the converted JSON
+              console.log(json);
+          }
+      })
+      .catch(function (error){
+          // Catch errors
+          console.log(error);
+      }); 
+    }
+  },
+
+  setup() {
+      const booking = ref({});
+      
+
+      return {
+          booking
+      }
   }
 }
 </script>
